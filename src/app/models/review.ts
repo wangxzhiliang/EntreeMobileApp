396c7c5f6bfd0ed41e9f58768de3afdc4ff2be51
xuyisuzy <e0424897@u.nus.edu>
2021-04-12 22:31:44 +0800

    merge

--- src/app/models/review.ts
+++ src/app/models/review.ts
@@ -1,2 +1,22 @@
+import { Customer } from "./customer";
+
 export class Review {
+    reviewId: number | undefined;
+    content: string | undefined;
+    rating: number | undefined;
+    photos: string[] | undefined;
+    numOfLikes: number | undefined;
+    timeOfCreation: Date | undefined;
+    creater: Customer | undefined;
+
+    constructor(reviewId?: number, content?: string, rating?: number, photos?: string[],
+        numOfLikes?: number, timeOfCreation?: Date, creater?: Customer) {
+        this.reviewId = reviewId;
+        this.content = content;
+        this.rating = rating;
+        this.photos = photos;
+        this.numOfLikes = numOfLikes;
+        this.timeOfCreation = timeOfCreation;
+        this.creater = creater;
+    }
 }
