dabd0335d48d3921793c7b1b4fa0faeeba93a413
xuyisuzy <e0424897@u.nus.edu>
2021-04-12 20:09:51 +0800

    review restaurant details wo reviews

--- src/app/models/dish.ts
+++ src/app/models/dish.ts
@@ -1,2 +1,18 @@
 export class Dish {
+    dishId: number | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    photo: string | undefined;
+    price: number | undefined;
+    recommended: boolean | undefined;
+
+    constructor(dishId?: number, name?: string, description?: string, photo?: string,
+        price?: number, recommended?: boolean) {
+        this.dishId = dishId;
+        this.name = name;
+        this.description = description;
+        this.photo = photo;
+        this.price = price;
+        this.recommended = recommended;
+    }
 }
