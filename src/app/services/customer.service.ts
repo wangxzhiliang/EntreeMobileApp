3b89ae131f8d9a5191eba4fc4e58814564bc42a8
nich-b <e0424778@u.nus.edu>
2021-04-12 10:43:01 +0800

    Update customer details

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -59,6 +59,7 @@ export class CustomerService
 		return throwError(errorMessage);		
 	}
 
+  //retrieveCustomerByEmail in rws
 	getCustomers(): Observable<Customer[]>
   {				
     return this.httpClient.get<Customer[]>(this.baseUrl + "/retrieveAllCustomers?username=" + this.sessionService.getUsername() + "&password=" + this.sessionService.getPassword()).pipe
@@ -85,8 +86,6 @@ export class CustomerService
     );
   }
 	
-	
-	
   updateCustomer(customerToUpdate: Customer): Observable<any>
   {
     let updateCustomerReq: UpdateCustomerReq = new UpdateCustomerReq(customerToUpdate);
@@ -96,8 +95,6 @@ export class CustomerService
       catchError(this.handleError)
     );
   }
-	
-  
   
   deleteCustomer(customerId: number): Observable<any>
   {
