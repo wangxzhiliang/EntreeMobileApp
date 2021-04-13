6e4994507cfcbf4454ffab72827e35de2f408b09
nich-b <e0424778@u.nus.edu>
2021-04-13 21:46:36 +0800

    model for sale transaction

---
+++ src/app/models/sale-transaction.spec.ts
@@ -0,0 +1,7 @@
+import { SaleTransaction } from './sale-transaction';
+
+describe('SaleTransaction', () => {
+  it('should create an instance', () => {
+    expect(new SaleTransaction()).toBeTruthy();
+  });
+});
